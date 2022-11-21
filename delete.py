76947fdd4bc931242d97faa5134c48ded97842c5
ospfpacket <61088091+ospfpacket@users.noreply.github.com>
2022-11-21 18:45:08 -0500

    Added status code handling

--- delete.py
+++ delete.py
@@ -14,9 +14,9 @@ class Delete(Command):
 
         message = message.split(' ')     #This section allows you to interact with the bot in a direct message or in a teams space
         if len(message) == 3:
-          message = message[2]
+            message = message[2]
         else:
-          message = message[1]
+            message = message[1]
 
         mac_addr = message
         
@@ -24,25 +24,30 @@ class Delete(Command):
 
         payload={}
         headers = {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': 'Basic64 Auth>'     #Basic Auth 64 is used
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic64 Auth>'     #Basic Auth 64 is used
         }
 
         response = requests.request("GET", url, headers=headers, data=payload, verify = False)
-        response = json.loads(response.text)
-        response = response.get('ERSEndPoint')
-        ep_id = (response['id'])
+        
+        print(response.status_code)     #This is for debugging in the console
+        
+        if response.status_code == 200:
+            response = json.loads(response.text)
+            response = response.get('ERSEndPoint')
+            ep_id = (response['id'])
+        elif response.status_code == 404:
+            return("Endpoint was not found in ISE. Please verify the MAC address and try again.")
+        else:
+            return("Unhandled Exception. Please contact your system administrator.")
 
         url = "https://<ise_server:port>/ers/config/endpoint/" + ep_id     #Put ISE Server name in URL with port
 
-        payload={}
-        headers = {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': '<Basic64 Auth>'     #Basic Auth 64 is used
-        }
-
         response = requests.request("DELETE", url, headers=headers, data=payload, verify = False)
-
-        return ("MAC Address " + mac_addr + " has been removed from ISE!")
+        if response.status_code == 204:
+            return ("MAC Address " + mac_addr + " has been removed from ISE!")
+        elif response.status_code == 404:
+            return("Endpoint was not found in ISE. Please verify the MAC address and try again.")
+        else:
+            return("Unhandled Exception. Please contact your system administrator.")
